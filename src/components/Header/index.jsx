0c8e94eceac2f67304cc611328ef815aff7a1995
nanonansen <buero@nanonansen.com>
2019-12-24 11:32:32 +0100

    Added React Headroom, updated navmenu styles

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,35 +1,14 @@
 import React, { useState } from "react";
-import useEventListener from "../../useEventListener";
+import Headroom from "react-headroom";
+
 import Logo from "../Logo";
 import NavMenu from "../NavMenu";
 import Icon from "../Icon";
 
 const Header = () => {
-    const [hideNav, setHideNav] = useState(false);
     const [showNav, setShowNav] = useState(false);
-    let lastScrollTop = window.scrollY;
-
-    const handleHeaderNavVisibility = () => {
-        if (lastScrollTop < window.scrollY) {
-            // Scrolling Down
-
-            // Hide Header Nav
-            if (window.scrollY > window.innerHeight && !showNav) {
-                setHideNav(true);
-            }
-        } else if (lastScrollTop > window.scrollY) {
-            // Scrolling Up
-
-            if (window.scrollY >= window.innerHeight) {
-                // Show Bottom Nav
-                setHideNav(false);
-            }
-        }
-        lastScrollTop = window.scrollY;
-    };
 
     const handleNavMenu = () => {
-        setHideNav(false);
         setShowNav(!showNav);
         if (!showNav) {
             document.body.classList.add("noscroll");
@@ -37,33 +16,28 @@ const Header = () => {
             document.body.classList.remove("noscroll");
         }
     };
-    useEventListener("scroll", handleHeaderNavVisibility);
 
     return (
-        <header
-            className={
-                hideNav
-                    ? "site-header site-header--sticky hidden"
-                    : "site-header site-header--sticky"
-            }
-        >
-            <div className="site-header__inner">
-                <div className="site-header__menu">
-                    <button onClick={handleNavMenu}>
-                        <Icon name={showNav ? "close" : "menu"} />
-                    </button>
+        <Headroom downTolerance={30}>
+            <header className="site-header">
+                <div className="site-header__inner">
+                    <div className="site-header__menu">
+                        <button onClick={handleNavMenu}>
+                            <Icon name={showNav ? "close" : "menu"} />
+                        </button>
+                    </div>
+                    <div className="branding">
+                        <h1 className="logo">
+                            <Logo />
+                        </h1>
+                    </div>
+                    <div className="site-header__cart">
+                        <button>1</button>
+                    </div>
                 </div>
-                <div className="branding">
-                    <h1 className="logo">
-                        <Logo />
-                    </h1>
-                </div>
-                <div className="site-header__cart">
-                    <button>1</button>
-                </div>
-            </div>
-            <NavMenu showNav={showNav} />
-        </header>
+                <NavMenu showNav={showNav} />
+            </header>
+        </Headroom>
     );
 };
 
